78cddd7d58969a577883b6955ef5d9e7c4bed213
Khkirill <97744368+Khkirill@users.noreply.github.com>
2023-03-01 23:56:43 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+import * as flsFunctions from "./modules/functions.js";
+import Swiper from 'swiper';
+import {
+	Navigation,
+	EffectFade,
+	Pagination,
+	Thumbs,
+	Controller,
+	FreeMode,
+	Scrollbar,
+	A11y,
+	Mousewheel
+} from 'swiper';
+import AOS from 'aos';
+
+import burger from './modules/burger.js';
+
+flsFunctions.isWebp();
